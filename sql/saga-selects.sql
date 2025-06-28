cb74c1a37b1dd0d0bd4f8937b2ce48aa18057d9e
Oriol Canalias <oriol.canalias@gmail.com>
2025-06-28 18:26:34 +0200

    saga selects

---
+++ sql/saga-selects.sql
@@ -0,0 +1,67 @@
+/**
+  Sagues principals finalitzades
+ */
+select name from saga where
+    total_main_titles = (select count(*) from book where saga_id = saga.id and saga_main_title is true);
+
+/**
+  Sagues principals + complementaries finalitzades
+ */
+select name from saga where
+      total_complementary_titles > 0 and
+    (total_main_titles + total_complementary_titles) = (select count(*) from book where saga_id = saga.id);
+
+
+/**
+  Sagues principals finalitzades però amb algun complementari no acabat
+ */
+select name from saga where
+      total_main_titles = (select count(*) from book where saga_id = saga.id and saga_main_title is true)
+      and (total_main_titles + total_complementary_titles) != (select count(*) from book where saga_id = saga.id);
+
+/**
+  Sagues principals no finalitzades
+ */
+select name from saga where total_main_titles > (select count(*) from book where saga_id = saga.id and saga_main_title is true);
+
+/**
+  Sagues iniciades el 2025
+ */
+select s.name from book
+  inner join public.reading r on book.id = r.book_id
+  inner join public.saga s on s.id = book.saga_id
+where
+    saga_id is not null and
+    saga_order = 1 and
+    date_part('year', date_read) = 2025;
+
+
+/**
+  Sagues finalitzades el 2025
+ */
+select s.name from book
+  inner join public.reading r on book.id = r.book_id
+  inner join public.saga s on s.id = book.saga_id
+where
+    saga_id is not null and
+    saga_main_title is true and
+    cast(saga_order as integer) = s.total_main_titles and
+    date_part('year', date_read) = 2025;
+
+/**
+  Sagues principals iniciades finalitzades el 2025
+ */
+select * from saga
+where 0 < (select count(1) from book
+     inner join public.reading r on book.id = r.book_id
+     where saga_main_title is true and
+          saga_id = saga.id and
+          saga_order = 1 and
+        date_part('year', date_read) = 2025) and
+    0 < (select count(1) from book
+     inner join public.reading r on book.id = r.book_id
+     where saga_main_title is true and
+         saga_id = saga.id and
+         cast(saga_order as integer) = saga.total_main_titles and
+         date_part('year', date_read) = 2025);
+

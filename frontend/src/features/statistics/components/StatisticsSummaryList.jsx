e635e816bdce5eee600579e25c2881ba67535c05
Oriol Canalias <oriol.canalias@gmail.com>
2025-10-22 17:01:58 +0200

    improving books dialog, using in statistic

---
+++ frontend/src/features/statistics/components/StatisticsSummaryList.jsx
@@ -0,0 +1,96 @@
+import {useState} from "react";
+import BookDetailsDialog from "@/features/books/components/BookDetailsDialog";
+
+export default function StatisticsSummaryList({stats}){
+  const [bookDetailsOpen, setBookDetailsOpen] = useState(false);
+  const [selectedBook, setSelectedBook] = useState(null);
+
+  const handleBookView = (bookId) => {
+    setSelectedBook(bookId);
+    setBookDetailsOpen(true);
+  };
+
+  return (
+    <>
+      <div className="grid md:grid-cols-2 gap-4 text-center md:text-left">
+        <div className="space-y-1">
+          <p className="text-lg font-medium">
+            Number of books:{" "}
+            <span className="font-bold">{stats.amountOfTotalReading}</span>
+          </p>
+          <p className="text-lg font-medium">
+            Total pages:{" "}
+            <span className="font-bold">{stats.totalPages}</span>
+          </p>
+        </div>
+        <div className="space-y-1">
+          <p className="text-lg font-medium">
+            Rereading:{" "}
+            <span className="font-bold">{stats.amountOfRereading}</span>
+          </p>
+          <p className="text-lg font-medium">
+            Average pages:{" "}
+            <span className="font-bold">
+                    {Math.round(stats.averagePages)}
+                  </span>
+          </p>
+        </div>
+        <div className="col-span-2 text-lg font-medium">
+          Average rating:{" "}
+          <span className="font-bold">
+                  {stats.averageRating.toFixed(2)}
+                </span>
+        </div>
+      </div>
+
+      <div>
+        <h3 className="text-xl font-semibold mb-2">
+          ⭐ Best books (rating {stats.bestBooks.rating})
+        </h3>
+        <div className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-4 gap-3">
+          {stats.bestBooks.bookList.map((book) => (
+            <div
+              key={book.id}
+              className="flex flex-col items-center text-center"
+            >
+              <img
+                src={book.urlImage}
+                alt={book.title}
+                className="w-28 h-40 object-cover rounded shadow cursor-pointer"
+                onClick={() => handleBookView(book.id)}
+              />
+              <p className="text-xs mt-2 line-clamp-2">{book.title}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div>
+        <h3 className="text-xl font-semibold mb-2">
+          💀 Worse books (rating {stats.worseBooks.rating})
+        </h3>
+        <div className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-5 gap-3">
+          {stats.worseBooks.bookList.map((book) => (
+            <div
+              key={book.id}
+              className="flex flex-col items-center text-center"
+            >
+              <img
+                src={book.urlImage}
+                alt={book.title}
+                className="w-28 h-40 object-cover rounded shadowcursor-pointer"
+                onClick={() => handleBookView(book.id)}
+              />
+              <p className="text-xs mt-2 line-clamp-2">{book.title}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+      <BookDetailsDialog
+        open={bookDetailsOpen}
+        onClose={setBookDetailsOpen}
+        bookId={selectedBook}
+      />
+    </>
+  );
+}
--- frontend/src/features/statistics/pages/StatisticsSummaryPage.jsx
+++ frontend/src/features/statistics/pages/StatisticsSummaryPage.jsx
@@ -9,6 +9,7 @@ import {
   SelectItem,
 } from "@/components/ui/select";
 import useStatisticsSummary from "@/features/statistics/hooks/useStatisticsSummary.js";
+import StatisticsSummaryList from "@/features/statistics/components/StatisticsSummaryList.jsx";
 
 export default function StatisticsSummaryPage() {
   const currentYear = new Date().getFullYear();
@@ -52,78 +53,8 @@ export default function StatisticsSummaryPage() {
 
         {stats && (
           <CardContent className="space-y-8">
-            <div className="grid md:grid-cols-2 gap-4 text-center md:text-left">
-              <div className="space-y-1">
-                <p className="text-lg font-medium">
-                  Number of books:{" "}
-                  <span className="font-bold">{stats.amountOfTotalReading}</span>
-                </p>
-                <p className="text-lg font-medium">
-                  Total pages:{" "}
-                  <span className="font-bold">{stats.totalPages}</span>
-                </p>
-              </div>
-              <div className="space-y-1">
-                <p className="text-lg font-medium">
-                  Rereading:{" "}
-                  <span className="font-bold">{stats.amountOfRereading}</span>
-                </p>
-                <p className="text-lg font-medium">
-                  Average pages:{" "}
-                  <span className="font-bold">
-                    {Math.round(stats.averagePages)}
-                  </span>
-                </p>
-              </div>
-              <div className="col-span-2 text-lg font-medium">
-                Average rating:{" "}
-                <span className="font-bold">
-                  {stats.averageRating.toFixed(2)}
-                </span>
-              </div>
-            </div>
-
-            <div>
-              <h3 className="text-xl font-semibold mb-2">
-                ⭐ Best books (rating {stats.bestBooks.rating})
-              </h3>
-              <div className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-4 gap-3">
-                {stats.bestBooks.bookList.map((book) => (
-                  <div
-                    key={book.id}
-                    className="flex flex-col items-center text-center"
-                  >
-                    <img
-                      src={book.urlImage}
-                      alt={book.title}
-                      className="w-28 h-40 object-cover rounded shadow"
-                    />
-                    <p className="text-xs mt-2 line-clamp-2">{book.title}</p>
-                  </div>
-                ))}
-              </div>
-            </div>
-
-            <div>
-              <h3 className="text-xl font-semibold mb-2">
-                💀 Worse books (rating {stats.worseBooks.rating})
-              </h3>
-              <div className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-5 gap-3">
-                {stats.worseBooks.bookList.map((book) => (
-                  <div
-                    key={book.id}
-                    className="flex flex-col items-center text-center"
-                  >
-                    <img
-                      src={book.urlImage}
-                      alt={book.title}
-                      className="w-28 h-40 object-cover rounded shadow"
-                    />
-                    <p className="text-xs mt-2 line-clamp-2">{book.title}</p>
-                  </div>
-                ))}
-              </div>
-            </div>
+            <StatisticsSummaryList
+              stats={stats}/>
           </CardContent>
         )}
       </Card>

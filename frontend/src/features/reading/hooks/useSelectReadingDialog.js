e403d2e5569d33c6ce8b9a5a2dbc0edc0de7f96e
Oriol Canalias <oriol.canalias@gmail.com>
2025-11-14 14:40:27 +0100

    useSelectReadingDialog first version

---
+++ frontend/src/features/reading/hooks/useSelectReadingDialog.js
@@ -0,0 +1,61 @@
+import {useEffect, useState} from "react";
+import {useDialog} from "@/context/DialogContext.jsx";
+import {getReadings} from "@/lib/api/readings.js";
+
+export default function useSelectReadingDialog(year, month) {
+  const [readings, setReadings] = useState({"content": []});
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [sortField, setSortField] = useState("DATE_READ");
+  const [sortDir, setSortDir] = useState("asc");
+
+  const filterDateFrom = `${year}-${month}-01`;
+  const filterDateTo = `${year}-${month}-${new Date(year, Number(month), 0).getDate()}`;
+
+  const { showError, showSuccess } = useDialog();
+
+  const fetchReadings = async () => {
+    setLoading(true);
+    const {data, error} = await getReadings({
+      page: page,
+      filterDateTo: filterDateTo,
+      filterDateFrom: filterDateFrom,
+      sortField: sortField,
+      sortDir: sortDir
+    })
+
+    if (error) {
+      showError(error);
+    }
+    if (data) {
+      setReadings(data.content);
+      setPage(data.page);
+      setTotalPages(data.totalPages);
+    }
+    setLoading(false);
+  };
+
+  const handleSort = (field) => {
+    if (sortField === field) {
+      setSortDir((prev) => (prev === "asc" ? "desc" : "asc"));
+    } else {
+      setSortField(field);
+      setSortDir("asc");
+    }
+  };
+
+  return {
+    readings,
+    loading,
+    page,
+    setPage,
+    totalPages,
+    filterDateTo,
+    filterDateFrom,
+    sortField,
+    sortDir,
+    handleSort,
+    fetchReadings
+  }
+}
